e22540e1a4a8389666bcd5d90a7b9bfed950d971
Thejashree-nagashree <thejashreev0209@gmail.com>
2025-04-07 19:26:24 +0530

    Changed the HTML for the site.

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,16 +1,14 @@
-"""
-WSGI config for mysite project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
+import sys
 import os
 
+# Add your project directory to the sys.path
+path = '/home/Thejashree/thejashree.pythonanywhere.com'
+if path not in sys.path:
+    sys.path.append(path)
+
+# Set environment variable for Django settings
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+
+# Activate virtual environment
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-
 application = get_wsgi_application()
